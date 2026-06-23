9162eb0d654088c6f4b864604f7c66c42bbd2fd3
Kat Berge <kat.n.berge@gmail.com>
2026-06-23 14:58:59 -0500

    adds capsule movement with keyboard input

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
 //! Shows how to create graphics that snap to the pixel grid by rendering to a texture in 2D
 
-use bevy::{
-    prelude::*,
-};
+use bevy::{material::key, math::VectorSpace, prelude::*};
 
 mod render;
+
+#[derive(Component)]
+struct Player;
+const MOVE_SPEED: f32 = 1.;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .add_systems(Startup, (render::setup_camera, setup_mesh))
-        .add_systems(Update, (rotate, render::fit_canvas))
+        .add_systems(Update, (rotate, move_player, render::fit_canvas))
         .run();
 }
-
 
 /// Spawns a capsule mesh on the pixel-perfect layer.
 fn setup_mesh(
@@ -22,9 +23,10 @@ fn setup_mesh(
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     commands.spawn((
+        Player,
         Mesh2d(meshes.add(Capsule2d::default())),
         MeshMaterial2d(materials.add(Color::BLACK)),
-        Transform::from_xyz(25., 0., 2.).with_scale(Vec3::splat(32.)),
+        Transform::from_xyz(0., 0., 0.).with_scale(Vec3::splat(8.)),
         render::Rotate,
         render::PIXEL_PERFECT_LAYERS,
     ));
@@ -38,3 +40,28 @@ fn rotate(time: Res<Time>, mut transforms: Query<&mut Transform, With<render::Ro
     }
 }
 
+fn move_player(
+    mut transforms: Query<&mut Transform, With<Player>>,
+    keys: Res<ButtonInput<KeyCode>>,
+) {
+    for mut transform in transforms.iter_mut() {
+        let mut direction = Vec3::ZERO;
+
+        if keys.pressed(KeyCode::KeyW) || keys.pressed(KeyCode::ArrowUp) {
+            direction.y += 1.0;
+        } // up
+        if keys.pressed(KeyCode::KeyS) || keys.pressed(KeyCode::ArrowDown) {
+            direction.y -= 1.0;
+        } // down
+        if keys.pressed(KeyCode::KeyD) || keys.pressed(KeyCode::ArrowRight) {
+            direction.x += 1.0;
+        } // right
+        if keys.pressed(KeyCode::KeyA) || keys.pressed(KeyCode::ArrowLeft) {
+            direction.x -= 1.0;
+        } // left
+
+        if 0.0 < direction.length() {
+            transform.translation += MOVE_SPEED * direction.normalize();
+        }
+    }
+}
