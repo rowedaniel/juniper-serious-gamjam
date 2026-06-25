167aaf3c68c55ad91933c5474f5dca917aca9fc1
Kat Berge <kat.n.berge@gmail.com>
2026-06-25 14:24:35 -0500

    adds basic art and changes player sprite

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,50 @@ mod render;
 #[derive(Component)]
 struct Player;
 const MOVE_SPEED: f32 = 50.;
+const CHARACTER_PIXEL_HEIGHT: u32 = 32;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
-        .add_systems(Startup, (render::setup_camera, setup_mesh))
+        .add_systems(Startup, (render::setup_camera, setup_character))
         .add_systems(Update, (rotate, move_player, render::fit_canvas))
         .run();
 }
 
-/// Spawns a capsule mesh on the pixel-perfect layer.
-fn setup_mesh(
+fn setup_character(
+    // mainly from https://medium.com/@thomsmed/animating-sprites-using-bevys-animationplayer-fa715d2c0815
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: ResMut<AssetServer>,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
 ) {
-    commands.spawn((
-        Player,
-        Mesh2d(meshes.add(Capsule2d::default())),
-        MeshMaterial2d(materials.add(Color::BLACK)),
-        Transform::from_xyz(0., 0., 0.).with_scale(Vec3::splat(8.)),
-        render::Rotate,
-        render::PIXEL_PERFECT_LAYERS,
+    // # Character
+    let character_name = Name::new("Character");
+    let character_entity = commands.spawn((Player, character_name.clone())).id();
+
+    let sprite_sheet_num_rows = 1;
+    let sprite_sheet_num_columns = 1; // increase when added sprites with animations
+    let sprite_sheet_section_size = CHARACTER_PIXEL_HEIGHT;
+
+    let texture_handle = asset_server.load("businessman.png");
+    let texture_atlas_layout = TextureAtlasLayout::from_grid(
+        UVec2::splat(sprite_sheet_section_size),
+        sprite_sheet_num_columns,
+        sprite_sheet_num_rows,
+        None,
+        None,
+    );
+    let texture_atlas_layout_handle = texture_atlas_layouts.add(texture_atlas_layout);
+
+    commands.entity(character_entity).insert((
+        character_name.clone(),
+        Sprite::from_atlas_image(
+            texture_handle.clone(),
+            TextureAtlas {
+                layout: texture_atlas_layout_handle.clone(),
+                index: 0,
+            },
+        ),
+        Transform::from_xyz(0.0, (CHARACTER_PIXEL_HEIGHT / 2) as f32, 0.0),
     ));
 }
 
